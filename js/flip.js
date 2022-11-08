93d73dee15d918a62909f90618c037cfa373ba06
Nicolas Franchini <devnicolasfranchini@gmail.com>
2022-11-07 23:15:18 -0300

    creacion de cards

---
+++ js/flip.js
@@ -0,0 +1,19 @@
+const card = document.querySelector(".card");
+card.addEventListener("click", () => {
+  card.classList.toggle("clicked");
+});
+
+const card1 = document.querySelector(".card1");
+card1.addEventListener("click", () => {
+  card1.classList.toggle("clicked");
+});
+
+const card2 = document.querySelector(".card2");
+card2.addEventListener("click", () => {
+  card2.classList.toggle("clicked");
+});
+
+const card3 = document.querySelector(".card3");
+card3.addEventListener("click", () => {
+  card3.classList.toggle("clicked");
+});
